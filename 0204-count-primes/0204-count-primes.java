360cffcd1e4216cbe7a33e6eabd5f1384384e8b3
omprakash kumar <omprakashkumarglb@gmail.com>
2025-10-31 12:52:44 +0530

    Time: 95 ms (66.69%), Space: 49.4 MB (37.51%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,34 +1,37 @@
 class Solution {
+
     public int countPrimes(int n) {
-         if (n <= 2) {
-            return 0; // Primes are greater than 1, so for n=0, 1, 2, there are no primes less than n.
-        }
+        if (n <=2)
+            return 0;
 
-        // Create a boolean array "is_prime[0..n-1]" and initialize all entries it as true.
-        // A value in is_prime[i] will finally be false if i is Not a prime, else true.
-        boolean[] is_prime = new boolean[n];
-        for (int i = 2; i < n; i++) {
-            is_prime[i] = true;
-        }
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime, true);
 
-        // Start from 2, mark multiples of prime numbers as false.
-        for (int p = 2; p * p < n; p++) {
-            // If is_prime[p] is still true, then it is a prime
-            if (is_prime[p]) {
-                // Mark all multiples of p as false
-                for (int i = p * p; i < n; i += p) {
-                    is_prime[i] = false;
-                }
+        isPrime[0] = isPrime[1] = false;
+
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i)
+                    isPrime[j] = false;
             }
         }
 
-        // Count prime numbers
+        int count = 0;
+        for (boolean prime : isPrime)
+            if (prime)
+                count++;
+
+        return count;
+    }
+
+    private boolean isPrime(int n) {
+        if (n < 2)
+            return false;
         int count = 0;
         for (int i = 2; i < n; i++) {
-            if (is_prime[i]) {
-                count++;
-            }
+            if (n % i == 0)
+                return false;
         }
-        return count;
+        return true;
     }
 }
