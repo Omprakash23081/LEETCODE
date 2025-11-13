cb9301d459bfbe57d37b0af929390230a58d5920
omprakash kumar <omprakashkumarglb@gmail.com>
2025-11-13 15:33:38 +0530

    Time: 525 ms (49.96%), Space: 0B (100%) - LeetHub

--- 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -1,9 +1,16 @@
-SELECT employee_id, department_id
-FROM Employee
-WHERE primary_flag = 'Y'
-   OR employee_id IN (
+SELECT e.employee_id, e.department_id
+FROM Employee e
+LEFT JOIN (
+    SELECT employee_id
+    FROM Employee
+    WHERE primary_flag = 'Y'
+    GROUP BY employee_id
+) p
+ON e.employee_id = p.employee_id
+WHERE e.primary_flag = 'Y'
+   OR (p.employee_id IS NULL AND e.employee_id IN (
         SELECT employee_id
         FROM Employee
         GROUP BY employee_id
         HAVING COUNT(*) = 1
-   );
+   ));
