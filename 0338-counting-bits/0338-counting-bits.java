26f5272ab56b2f9d844dbec22ae7e876b69b0f81
omprakash kumar <omprakashkumarglb@gmail.com>
2025-11-13 21:51:19 +0530

    Time: 3 ms (29.82%), Space: 48.7 MB (5.05%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,29 +1,15 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] lookup = new int[n+1];
-        for (int i = 0; i < n+1; i++) {
-            lookup[i] = lookup[i >> 1] + (i & 1);
+        int[] ans = new int[n + 1];
+        int offset = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if (offset * 2 == i) {
+                offset = i;   
+            }
+            ans[i] = 1 + ans[i - offset];
         }
-        return lookup;
+
+        return ans;
     }
 }
-// int arr[] = new int[n + 1];
-// int j = 1;
-// int count =4;
-// int temp=0;
-// arr[0]=0;
-// for (int i = 1; i <= n; i++) {
-//    if(i%count==0){
-//      arr[i]=1;
-//    }
-//    else if((i+1)%count==0){
-//      arr[i] = i/count+2+temp;
-//    }else{
-//      arr[i]=i/count+1+temp;
-//    }
-//    if(i>=count){
-//      count = count*2;
-//      temp++;
-//    }
-// }
-// return arr;
