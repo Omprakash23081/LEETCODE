3ac8a115c0d179996323edd7e6c1628d3761fadf
omprakash kumar <omprakashkumarglb@gmail.com>
2025-11-20 16:00:37 +0530

    Time: 436 ms (25.97%), Space: 0B (100%) - LeetHub

---
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -0,0 +1,15 @@
+-- SELECT num
+-- FROM MyNumbers
+-- GROUP BY num
+-- HAVING COUNT(*) = 1
+-- ORDER BY num DESC
+-- LIMIT 1;
+
+SELECT 
+    (SELECT num 
+     FROM MyNumbers
+     GROUP BY num
+     HAVING COUNT(*) = 1
+     ORDER BY num DESC
+     LIMIT 1)
+AS num;
