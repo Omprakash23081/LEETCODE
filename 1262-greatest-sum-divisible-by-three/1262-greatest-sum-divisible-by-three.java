35947b0d476d5320c0a6cafb9a02a67129bd355a
omprakash kumar <omprakashkumarglb@gmail.com>
2025-11-23 15:19:20 +0530

    Time: 4 ms (94.72%), Space: 49.5 MB (45.34%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
@@ -1,37 +1,50 @@
-import java.util.*;
-
 class Solution {
     public int maxSumDivThree(int[] nums) {
         int total = 0;
-        ArrayList<Integer> mod1 = new ArrayList<>();
-        ArrayList<Integer> mod2 = new ArrayList<>();
+
+        int mod10 = Integer.MAX_VALUE, mod11 = Integer.MAX_VALUE;
+        int mod20 = Integer.MAX_VALUE, mod21 = Integer.MAX_VALUE;
 
         for (int a : nums) {
             total += a;
             int r = a % 3;
-            if (r == 1) mod1.add(a);
-            else if (r == 2) mod2.add(a);
+
+            if (r == 1) {
+                if (a < mod10) {
+                    mod11 = mod10;
+                    mod10 = a;
+                } else if (a < mod11) 
+                    mod11 = a;
+            }
+
+            else if (r == 2) {
+                if (a < mod20) {
+                    mod21 = mod20;
+                    mod20 = a;
+                } else if (a < mod21)
+                    mod21 = a;
+            }
         }
 
-        if (total % 3 == 0) return total;
-
-        Collections.sort(mod1);
-        Collections.sort(mod2);
+        if (total % 3 == 0)
+            return total;
 
         int rem = total % 3;
-        int option1 = Integer.MAX_VALUE; 
-        int option2 = Integer.MAX_VALUE; 
+        int option1 = Integer.MAX_VALUE, option2 = Integer.MAX_VALUE;
 
         if (rem == 1) {
-            if (!mod1.isEmpty()) option1 = mod1.get(0);
-            if (mod2.size() >= 2) option2 = mod2.get(0) + mod2.get(1);
+            if (mod10 != Integer.MAX_VALUE)
+                option1 = mod10;
+            if (mod20 != Integer.MAX_VALUE && mod21 != Integer.MAX_VALUE)
+                option2 = mod20 + mod21;
         } else {
-            if (!mod2.isEmpty()) option1 = mod2.get(0);
-            if (mod1.size() >= 2) option2 = mod1.get(0) + mod1.get(1);
+            if (mod20 != Integer.MAX_VALUE)
+                option1 = mod20;
+            if (mod10 != Integer.MAX_VALUE && mod11 != Integer.MAX_VALUE)
+                option2 = mod10 + mod11;
         }
 
         int toRemove = Math.min(option1, option2);
-        if (toRemove == Integer.MAX_VALUE) return 0; 
         return total - toRemove;
     }
 }
