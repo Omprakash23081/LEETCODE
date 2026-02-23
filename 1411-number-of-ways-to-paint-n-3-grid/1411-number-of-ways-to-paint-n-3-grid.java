7a8715e10d8a1e8f855d7cf1de9b6ff5eea0512a
omprakash kumar <omprakashkumarglb@gmail.com>
2026-02-23 13:22:41 +0530

    Time: 112 ms (5.18%), Space: 47 MB (5.84%) - LeetHub

---
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.java
@@ -0,0 +1,68 @@
+class Solution {
+    int MOD = 1000000007;
+    List<String> li;
+    Integer dp[][];
+
+    public int numOfWays(int n) {
+        li = new ArrayList<>();
+        combination(new StringBuilder());
+
+        dp = new Integer[n][li.size()];
+
+        int res = 0;
+
+        for (int i = 0; i < li.size(); i++) {
+            res = (res + solve(n - 1, i)) % MOD;
+        }
+
+        return res;
+    }
+
+    int solve(int rem, int prevState) {
+
+        if (rem == 0)
+            return 1;
+
+        if (dp[rem][prevState] != null)
+            return dp[rem][prevState];
+
+        String prev = li.get(prevState);
+
+        int ways = 0;
+
+        for (int i = 0; i < li.size(); i++) {
+
+            String curr = li.get(i);
+            boolean valid = true;
+
+            for (int j = 0; j < 3; j++) {
+                if (prev.charAt(j) == curr.charAt(j)) {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (valid) {
+                ways = (ways + solve(rem - 1, i)) % MOD;
+            }
+        }
+
+        return dp[rem][prevState] = ways;
+    }
+
+    void combination(StringBuilder s) {
+
+        if (s.length() == 3) {
+            li.add(s.toString());
+            return;
+        }
+
+        for (char ch : new char[] { 'r', 'g', 'b' }) {
+            if (s.length() == 0 || s.charAt(s.length() - 1) != ch) {
+                s.append(ch);
+                combination(s);
+                s.deleteCharAt(s.length() - 1);
+            }
+        }
+    }
+}
