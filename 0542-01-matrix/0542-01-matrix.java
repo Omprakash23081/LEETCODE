9bf6f727a5bca01463f855ccd80ba9e52030380d
omprakash kumar <omprakashkumarglb@gmail.com>
2026-03-19 20:53:29 +0530

    Time: 15 ms (45.11%), Space: 48.8 MB (79%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,50 +1,46 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int grid[][] = mat;
-        int m = grid.length;
-        int n = grid[0].length;
-        int arr[][] = new int[m][n];
-        boolean visited[][] = new boolean[m][n];
+        int n = mat.length;
+        int m = mat[0].length;
+        boolean visited[][] = new boolean[n][m];
         Queue<int[]> qu = new LinkedList<>();
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 0) {
-                    qu.add(new int[] { i, j, 0 });
+        for(int i = 0 ; i<n ; i++){
+            for(int j = 0 ; j<m ; j++){
+                if(mat[i][j]==0) {
+                    qu.add(new int[]{i , j , 0});
                     visited[i][j] = true;
                 }
             }
         }
 
-        while (!qu.isEmpty()) {
+        while(!qu.isEmpty()){
             int temp[] = qu.poll();
             int i = temp[0];
             int j = temp[1];
             int wat = temp[2];
 
-            if (i + 1 < m && !visited[i + 1][j]) {
-                arr[i + 1][j] = wat + 1;
-                qu.add(new int[] { i + 1, j, wat + 1 });
-                visited[i + 1][j] = true;
+            if(i+1<n && !visited[i+1][j]){
+                mat[i+1][j] = wat+1;
+                qu.add(new int[]{i+1 , j , wat+1});
+                visited[i+1][j] = true;
             }
-            if (i - 1 >= 0 && !visited[i - 1][j]) {
-                arr[i - 1][j] = wat + 1;
-                qu.add(new int[] { i - 1, j, wat + 1 });
-                visited[i - 1][j] = true;
-
+            if(i-1>=0 && !visited[i-1][j]){
+                mat[i-1][j] = wat+1;
+                qu.add(new int[]{i-1 , j , wat+1});
+                visited[i-1][j] = true;
             }
-            if (j + 1 < n && !visited[i][j + 1]) {
-                arr[i][j + 1] = wat + 1;
-                qu.add(new int[] { i, j + 1, wat + 1 });
-                visited[i][j + 1] = true;
-
+            if(j+1<m && !visited[i][j+1]){ 
+                mat[i][j+1] = wat+1;
+                qu.add(new int[]{i , j+1 , wat+1});
+                visited[i][j+1] = true;
             }
-            if (j - 1 >= 0 && !visited[i][j - 1]) {
-                arr[i][j - 1] = wat + 1;
-                qu.add(new int[] { i, j - 1, wat + 1 });
-                visited[i][j - 1] = true;
+            if(j-1>=0 && !visited[i][j-1]){
+                mat[i][j-1] = wat+1;
+                qu.add(new int[]{i , j-1 , wat+1});
+                visited[i][j-1] = true;
             }
         }
-        return arr;
+        return mat;
     }
 }
