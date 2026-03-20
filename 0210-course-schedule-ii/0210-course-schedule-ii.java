476d89634406dadab82b88cffca7ea94ae62b09b
omprakash kumar <omprakashkumarglb@gmail.com>
2026-03-20 21:41:47 +0530

    Time: 5 ms (82.21%), Space: 46.7 MB (88.66%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,49 +1,47 @@
+import java.util.*;
+
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        int freq[] = new int[numCourses];
-        List<List<Integer>> li = new ArrayList<>();
-        Stack<Integer> st = new Stack<>();
-        int ans[] = new int[numCourses];
+        int indegree[] = new int[numCourses];
+        List<List<Integer>> graph = new ArrayList<>();
 
-        for (int arr[] : prerequisites)
-            freq[arr[1]]++;
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
 
-        for (int i = 0; i < numCourses; i++)
-            li.add(new ArrayList<>());
+        for (int[] arr : prerequisites) {
+            int a = arr[0], b = arr[1];
+            graph.get(b).add(a);   
+            indegree[a]++;        
+        }
 
-        for (int arr[] : prerequisites)
-            li.get(arr[0]).add(arr[1]);
+        Queue<Integer> q = new LinkedList<>();
 
-        for (int i = 0; i < numCourses; i++)
-            if (freq[i] == 0)
-                st.push(i);
-
-        int count = 0;
-
-        while (!st.isEmpty()) {
-            int v = st.pop();
-            for (int val : li.get(v)) {
-                freq[val]--;
-                if (freq[val] == 0)
-                    st.push(val);
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.offer(i);
             }
-            ans[count++] = v;
-        }
-        count = 0;
-        for(int v : ans) if(v==0) count++;
-
-        if(count > 1) return new int[]{};
-
-        int left = 0;
-        int right = numCourses - 1;
-        while (left < right) {
-            int temp = ans[left];
-            ans[left] = ans[right];
-            ans[right] = temp;
-            left++;
-            right--;
         }
 
-        return ans;  
+        int ans[] = new int[numCourses];
+        int index = 0;
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            ans[index++] = node;
+
+            for (int nei : graph.get(node)) {
+                indegree[nei]--;
+                if (indegree[nei] == 0) {
+                    q.offer(nei);
+                }
+            }
+        }
+
+        if (index != numCourses) {
+            return new int[]{}; 
+        }
+
+        return ans;
     }
 }
