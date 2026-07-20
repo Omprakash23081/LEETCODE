e83ee904c2625f70961d573e349cd5197d940d54
omprakash kumar <omprakashkumarglb@gmail.com>
2026-07-21 00:09:49 +0530

    Time: 1 ms (23.32%), Space: 42.7 MB (13.37%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -1,24 +1,23 @@
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> li = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        solve(li , temp , 0 , k , n , 1);
+        solve(li, new ArrayList<>(), k, n, 1);
         return li;
     }
 
-    private void solve(List<List<Integer>> li , List<Integer> temp , int sum , int k  , int n ,int idx){
-        if(temp.size()>=k && sum == n){
-            li.add(new ArrayList<>(temp));
-            return ;
+    void solve(List<List<Integer>> li, List<Integer> res, int k, int n , int idx) {
+        if (k == res.size() && 0 == n) {
+            li.add(new ArrayList<>(res));
+            return;
         }
-        if(temp.size()>= k) return ;
+        if (n <= 0 || n < 0)
+            return;
 
-        for(int i  = idx ; i<10 ; i++){
-            int size = temp.size();
-            if(size>0 && temp.get(size-1) == i) continue;
-            temp.add(i);
-            solve(li , temp , sum+i , k , n , i);
-            temp.remove(temp.size()-1);
+        for (int i = idx; i <= 9; i++) {
+            res.add(i);
+            solve(li, res, k, n-i , i+1);
+            if (res.size() > 0)
+                res.remove(res.size() - 1);
         }
     }
 }
