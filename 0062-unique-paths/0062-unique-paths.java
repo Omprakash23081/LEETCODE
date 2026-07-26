7f5b084a5ed6dc0f7d91c162afa8ab821d5a41b9
omprakash kumar <omprakashkumarglb@gmail.com>
2026-07-26 23:23:55 +0530

    Time: 1 ms (14.55%), Space: 42.4 MB (13.21%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,17 +1,38 @@
 class Solution {
-    
-    private int helper(int arr[][] , Integer DP[][] , int row , int cal){
-        if(row>arr.length || row<0 || cal>arr[0].length || cal<0){
-            return 0;
-        }
-        if(row==arr.length-1 && cal==arr[0].length-1)return 1;
-        if(DP[row][cal]!=null)return DP[row][cal];
-        return DP[row][cal] = helper(arr , DP , row+1 , cal) + helper(arr , DP , row , cal+1);
+    public int uniquePaths(int m, int n) {
+        int arr[][] = new int[m][n];
+        boolean visited[][] = new boolean[m][n];
+        return optmil_solve(arr);
     }
 
-    public int uniquePaths(int m, int n) {
-        int arr[][] = new int[m][n]; 
-        Integer DP[][] = new Integer[m+1][n+1]; 
-        return helper(arr ,DP , 0 , 0);
+    int solve(int arr[][] , boolean visited[][] , int i , int j){
+        int m = arr.length;
+        int n = arr[0].length;
+        if(i == m-1 && j == n-1) return 1;
+        if(i >= m || j>=n || i<0 || j<0) return 0;
+        if(visited[i][j]) return 0;
+
+        visited[i][j] = true;
+
+        int c1 = solve(arr , visited , i+1 , j);
+        int c2 = solve(arr , visited , i , j+1);
+
+        visited[i][j] = false;
+
+        return c1+c2;
+    }
+
+    int optmil_solve(int arr[][]){
+        int m = arr.length;
+        int n = arr[0].length;
+        int dp[][] = new int[m+1][n+1];
+        dp[1][1] = 1;
+
+        for(int i = 1 ; i<=m ; i++){
+            for(int j = 1 ; j<=n ; j++){
+                dp[i][j] += dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m][n];
     }
 }
